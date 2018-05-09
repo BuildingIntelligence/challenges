b97e87f0a33edabdeed948e2747a6f2cc58c771f
Thomas Chang <Tomchang93@gmail.com>
2018-05-08 21:06:12 -0400

    finished challenged

--- loginpage/login_page.rb
+++ loginpage/login_page.rb
@@ -1,5 +1,5 @@
 require 'sinatra/base'
-
+require 'pry'
 class LoginPage < Sinatra::Base
 	set :public_folder, File.dirname(__FILE__) + '/public'
 
@@ -7,14 +7,33 @@ class LoginPage < Sinatra::Base
 		erb :index
 	end
 
+	get('/forgot_password') do
+		[200, "forgot password"]
+	end
+
 	post('/login') do
 		puts "   Email: #{params[:email]}"
 		puts "Password: #{params[:password]}"
-
-		if true
-			[200, "success"]
+		
+		if (params[:email] =="admin@example.com" && params[:password] == "password123")
+			@user = "admin@example.com"
 		else
-			[400, "failure"]
+			@error = "Unable to find user, please try again."
 		end
+		if request.xhr?
+			if @user
+				status 200
+				erb :'partials/_success', layout: false, locals: {user: @user}
+			else
+				erb :'partials/_failure', layout: false, locals: {error: @error}
+			end
+		else
+			if @user
+				[200, "successfully logged in"]
+			else
+				[400, "Unable to find user."]
+			end
+		end
+
 	end
 end
